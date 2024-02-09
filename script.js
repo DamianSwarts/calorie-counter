3887001dbf7d0563f8fd8e084a6efbfed7653301
Damian Swarts <damianswarts2019@gmail.com>
2024-02-09 19:14:02 +0200

    Initialized a function to clean the string input by the user and ensure a number

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
-const output = document.getElementById('output'); 
+const output = document.getElementById('output');
+let isError = false; //Variable initialized for later use in the program
+
+// Function to clean the string input by the user and ensure a number.
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+} 
