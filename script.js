f5957dac296e880c3c23b52736184ed9455f6427
Damian Swarts <damianswarts2019@gmail.com>
2024-02-09 18:45:46 +0200

    Accessed various elements from index.html using their id attributes to use in the JavaScript code

---
+++ script.js
@@ -0,0 +1,7 @@
+// Accessing various elements in the HTML with their given ids dto use in the JS code.
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output'); 
